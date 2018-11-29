76d7eb931d3992cfe590d29be3dcb731d27846a6
qashmin <qashcoin@protonmail.com>
2018-11-29 13:03:54 -0500

    seednode

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -147,7 +147,7 @@ const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "5617aed189361562fcc3093f3560a161683adb1dc127c63a7e05a0bab85117e0";
 
 const char* const SEED_NODES[] = {
-  "127.0.0.1:2045"
+  "50.116.0.79:2233"
 };
 
 struct CheckpointData {
