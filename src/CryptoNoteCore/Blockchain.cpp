247a279a77b4330f9ff29734259a44e211532caa
qashmin <qashcoin@protonmail.com>
2018-11-29 16:41:33 -0500

    adjust block parameter

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -1073,7 +1073,7 @@ bool Blockchain::validate_miner_transaction(const Block& b, uint32_t height, siz
     return false;
   }
 
-  if (minerReward > reward && height > 1) {
+  if (minerReward > reward &&) {
     logger(ERROR, BRIGHT_RED) << "Coinbase transaction attempted to spend too much money: " << m_currency.formatAmount(minerReward) <<
       ", block reward is " << m_currency.formatAmount(reward);
     return false;
